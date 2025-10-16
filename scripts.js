c4f99716abadf8515aa1881ebe0e6295648b5537
ProudMay <mehdi79nekouei@gmail.com>
2025-10-16 22:10:49 +0330

    fix[Grid section ]
    now grids container work good and user can enter a number after that container fits to user grids input

--- scripts.js
+++ scripts.js
@@ -1,31 +1,27 @@
 const container = document.querySelector(".container");
-const defaultGrid = document.createDocumentFragment();
-const gridSize = 16;
-const containerSize = container.offsetWidth;
-const cellSize = containerSize / gridSize;
-
-
 const submitBtm = document.querySelector(".sub button");
 
 function grids(gridSize) {
-  const size = +gridSize;
-  if (size <= 0 || size > 100) {
-    alert("enter a number between 1 and 100");
-    return;
-  }
-  container.innerHTML = "";
-
-  for (let row = 0; row < 16; row++) {
-    for (let col = 0; col < 16; col++) {
+    const size = +gridSize;
+    if (size <= 0 || size > 100) {
+        alert("enter a number between 1 and 100");
+        return;
+    }
+    container.innerHTML = "";
+    const containerSize = container.offsetWidth;
+    const cellSize = containerSize / size;
+    const newGridFragment = document.createDocumentFragment();
+  for (let row = 0; row < size; row++) {
+    for (let col = 0; col < size; col++) {
       const defCells = document.createElement("div");
       defCells.className = "default-cells";
       defCells.style.height = `${cellSize}px`;
       defCells.style.width = `${cellSize}px`;
-      defaultGrid.appendChild(defCells);
+      newGridFragment.appendChild(defCells);
     }
     
 }
-container.appendChild(defaultGrid);
+container.appendChild(newGridFragment);
 
 }
 grids(16);
@@ -34,5 +30,5 @@ const newGridSize = prompt("Enter a number for your grids (between 1 and 100):)"
     if (newGridSize === null || newGridSize.trim() === '') {
         return;
     }
-    createGrid(newGridSize);
+    grids(newGridSize);
 });
